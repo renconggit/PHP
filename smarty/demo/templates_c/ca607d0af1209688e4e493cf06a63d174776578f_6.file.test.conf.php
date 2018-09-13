2199856e5d896252eecaf7f0a3e95eb5c07cf6de
renconggit <865506898@qq.com>
2018-09-13 18:18:53 +0800

    Signed-off-by: renconggit <865506898@qq.com>

---
+++ smarty/demo/templates_c/ca607d0af1209688e4e493cf06a63d174776578f_6.file.test.conf.php
@@ -0,0 +1,44 @@
+<?php /* Smarty version 3.1.32, created on 2018-09-13 10:18:27
+         compiled from 'C:\wamp64\www\php\smarty\demo\configs\test.conf' */ ?>
+<?php
+/* Smarty version 3.1.32, created on 2018-09-13 10:18:27
+  from 'C:\wamp64\www\php\smarty\demo\configs\test.conf' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.32',
+  'unifunc' => 'content_5b9a3973005392_13608026',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'ca607d0af1209688e4e493cf06a63d174776578f' => 
+    array (
+      0 => 'C:\\wamp64\\www\\php\\smarty\\demo\\configs\\test.conf',
+      1 => 1536831553,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5b9a3973005392_13608026 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->smarty->ext->configLoad->_loadConfigVars($_smarty_tpl, array (
+  'sections' => 
+  array (
+    'setup' => 
+    array (
+      'vars' => 
+      array (
+        'bold' => true,
+      ),
+    ),
+  ),
+  'vars' => 
+  array (
+    'title' => 'Welcome to Smarty!!!',
+    'cutoff_size' => 40,
+  ),
+));
+}
+}
